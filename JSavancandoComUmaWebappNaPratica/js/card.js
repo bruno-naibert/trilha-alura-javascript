ed8228040e419721ee6b2b4acff7bde445333289
Bruno Naibert <bruno.naibert@rcadigital.com.br>
2017-04-24 17:15:36 -0300

    Ae! Vamos por a mão no JavaScript 5

--- JSavancandoComUmaWebappNaPratica/js/card.js
+++ JSavancandoComUmaWebappNaPratica/js/card.js
@@ -2,7 +2,11 @@ var $cardColors = document.querySelectorAll('.card-colors');
 
 for (var index = 0; index < $cardColors.length; index++) {
 	$cardColors[index].addEventListener('click', function() {
-		console.log('voce clicou')
+		var $card = this.parentNode.parentNode.parentNode;
+
+		console.log($card.dataset.color);
+
+		$card.dataset.color = this.dataset.color;
 	});
 };
 
