fd6260b7160feb1bfbd5db10ebf775dfdda9e5e4
Bruno Naibert <bruno.naibert@rcadigital.com.br>
2017-04-24 17:52:19 -0300

    Ae! Vamos por a mão no JavaScript 7

--- JSavancandoComUmaWebappNaPratica/js/card.js
+++ JSavancandoComUmaWebappNaPratica/js/card.js
@@ -1,15 +1,23 @@
+var $cards = document.querySelectorAll('.card');
 var $cardColors = document.querySelectorAll('.card-colors');
 
-for (var index = 0; index < $cardColors.length; index++) {
-	$cardColors[index].addEventListener('click', function() {
-		var $card = this.parentNode.parentNode.parentNode;
+for (var index = 0; index < $cards.length; index++) {
 
-		$card.dataset.color = this.dataset.color;
-		for (var position = 0; position < $cardColors.length; position++) {
-			$cardColors[position].classList.remove('isActive');
+	$cards[index].addEventListener('click', function(event) {
+
+		var $this = event.target;
+		if ($this.classList.contains('card-colors')) {
+			var $card = this;
+
+			$card.dataset.color = $this.dataset.color;
+
+			for (var position = 0; position < $cardColors.length; position++) {
+				$cardColors[position].classList.remove('isActive');
+			}
+			$this.classList.add('isActive');
+			
 		}
-
-		this.classList.add('isActive');
-	});
+		
+	})
 };
 
