e3150621f1857abdc1a0011f4de2d157313c4dff
Bruno Naibert <brunonaibert@hotmail.com>
2017-04-26 20:10:57 -0300

    Implementado HTML, CSS e JavaScritpt pra criar novos cartões 8

--- JSavancandoComUmaWebappNaPratica/js/card.js
+++ JSavancandoComUmaWebappNaPratica/js/card.js
@@ -1,39 +1,36 @@
-var $cards = document.querySelectorAll('.card');
+var $wrapCard = document.querySelector('.wrap-card');
 var $cardColors = document.querySelectorAll('.card-options');
 
-for (var index = 0; index < $cards.length; index++) {
+	$wrapCard.addEventListener('click', function(event) {
 
-	$cards[index].addEventListener('click', function(event) {
+	var $this = event.target;
+	var $card = $this.parentNode.parentNode.parentNode;
+	var $cardContent = $card.querySelector('.card-content');
 
-		var $this = event.target;
-		var $card = this;
-		var $cardContent = $card.querySelector('.card-content');
+	if ($this.dataset.color) {
 
-		if ($this.dataset.color) {
+		$card.dataset.color = $this.dataset.color;
 
-			$card.dataset.color = $this.dataset.color;
+		for (var position = 0; position < $cardColors.length; position++) {
+			$cardColors[position].classList.remove('isActive');
+		}
+		$this.classList.add('isActive');
+	};
 
-			for (var position = 0; position < $cardColors.length; position++) {
-				$cardColors[position].classList.remove('isActive');
-			}
+	if ($this.classList.contains('card_delete')) {
+
+		$card.remove();
+	};
+
+	if ($this.classList.contains('card_edit')) {
+		if ($cardContent.getAttribute('contenteditable') == 'false') {
+			$cardContent.setAttribute('contenteditable', 'true');
+			$cardContent.focus();
 			$this.classList.add('isActive');
+		} else {
+			$cardContent.setAttribute('contenteditable', 'false');
+			$cardContent.blur();
+			$this.classList.remove('isActive');
 		};
-
-		if ($this.classList.contains('card_delete')) {
-
-			$card.remove();
-		};
-
-		if ($this.classList.contains('card_edit')) {
-			if ($cardContent.getAttribute('contenteditable') == 'false') {
-				$cardContent.setAttribute('contenteditable', 'true');
-				$cardContent.focus();
-				$this.classList.add('isActive');
-			} else {
-				$cardContent.setAttribute('contenteditable', 'false');
-				$cardContent.blur();
-				$this.classList.remove('isActive');
-			};
-		};
-	});
-};
+	};
+});
